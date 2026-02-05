edc03ea62873e534676a4a84e46dbbb1b816787f
paulChevalier78 <paul.chevalier@edu.ece.fr>
2026-02-05 12:22:15 +0100

    Fix configure.js to properly handle environment variables and convert port to number

--- lab/src/configure.js
+++ lab/src/configure.js
@@ -3,10 +3,11 @@ const config_default = require('../conf/default')
 
 module.exports = (config_custom = {}) => {
   // Allow environment variables to override default config
-  const config_env = {
-    redis: {
+  const config_env = {}
+  if (process.env.REDIS_HOST || process.env.REDIS_PORT) {
+    config_env.redis = {
       host: process.env.REDIS_HOST || config_default.redis.host,
-      port: process.env.REDIS_PORT || config_default.redis.port
+      port: parseInt(process.env.REDIS_PORT) || config_default.redis.port
     }
   }
   const config = mixme.merge(config_default, config_env, config_custom)
